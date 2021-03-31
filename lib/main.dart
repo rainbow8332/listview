0fe6065f940836661075e5bcdef77bddb08808da
TaeHeun Lee <nixstory@gmail.com>
2021-03-31 15:14:10 +0900

    test 4

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Flutter Application"),
+          title: Text("Flutter Application 2"),
         ),
         body: Center(child: RandomWords()),
       ),
